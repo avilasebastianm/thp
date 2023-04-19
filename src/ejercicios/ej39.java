45c134436eb3739e94741ecbfba4b2586a7d7e43
avila.sebastianm <avila.sebastianm@gmail.com>
2023-04-19 09:48:34 -0300

    Ej39 correccion

--- src/ejercicios/ej39.java
+++ src/ejercicios/ej39.java
@@ -12,26 +12,6 @@ public static final int CANT_MIN_PARTICIPANTES = 2;
     public static final int PUNTOS_MIN = 100;
     public static final int SIN_PUNTOS = 0;
     public static void main(String[] args) {
-                  /*  .Nos proponemos desarrollar un “Tiro al blanco” en el cual 2 o más participantes realizan 3
-                    disparos consecutivos cada uno. Cada tiro, dependiendo de la distancia al centro da un
-                    puntaje que se acumula (se suman los tres disparos). Gana el jugador con mayor puntaje (se
-                    supone único).
-                    La puntuación para cada tiro es la siguiente:
-                         ● Si la distancia respecto al centro es 0 se ganan 500 puntos;
-                         ● Si la distancia es <= a 10, se ganan 250;
-                         ● Si la distancia está entre 11 y 50 ganará 100 puntos;
-                         ● Si es mayor no ganará nada (cayó fuera del tablero).
-                    La mecánica del juego es la siguiente:
-                    Se pide la cantidad de jugadores (mayor o igual a 2).
-                    Por cada jugador:
-                          ● Ingresar el nombre a cero), calculando para cada uno de estos el puntaje obtenido.
-                    Se pide:
-                          ● Informar el nombre del participante ganador del torneo y su puntaje (suponer único máximo)
-                          ● Informar la cantidad total de tiros al  */
-
-
-
-
         String nombre;
         String nombreGanador = "";
         int distancia;
